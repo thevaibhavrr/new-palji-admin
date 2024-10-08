f044b97f740a6a9910858f311a9d93dc9a096319
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-10-08 09:15:56 +0530

    update Url

--- src/api/callApi.jsx
+++ src/api/callApi.jsx
@@ -22,7 +22,9 @@ export const makeApi = async (
       method,
       // url: `https://pajiweb.onrender.com${endpoint}`,
       // url: `https://sk-backend-uvv5.onrender.com${endpoint}`,
-      url:"http://localhost:7000"+endpoint,
+      // url:"http://localhost:7000"+endpoint,
+      url: `https://new-palji-backend-1.onrender.com${endpoint}`,
+
       headers,
       data
     };
