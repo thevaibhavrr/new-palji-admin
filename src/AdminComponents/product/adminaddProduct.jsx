89cf27d82cd34c9ff5505d823eb68c5a954d35f5
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-10-09 13:49:01 +0530

    catorgry update

--- src/AdminComponents/product/adminaddProduct.jsx
+++ src/AdminComponents/product/adminaddProduct.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../../adminCss/product/adminaddProduct.css";
 import { makeApi } from "../../api/callApi";
-import axios from "axios";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
